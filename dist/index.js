7795adeb3e3e0ff6576f4fa55556f0562c4dea0e
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-23 22:06:11 +0300

    connected to postgres

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
-console.log('Testing the typescript config');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const typeorm_1 = require("typeorm");
+const main = async () => {
+    try {
+        await (0, typeorm_1.createConnection)({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'oscar',
+            password: 'password',
+            database: 'oscar',
+        });
+        console.log('connected to postgres');
+    }
+    catch (error) {
+        console.log('unable to connect to postgres');
+    }
+};
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Testing the typescript config');
+import { createConnection } from 'typeorm';
+
+const main = async () => {
+  try {
+    await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'oscar',
+      password: 'password',
+      database: 'oscar',
+    });
+
+    console.log('connected to postgres');
+  } catch (error) {
+    console.log('unable to connect to postgres');
+  }
+};
+
+main();
