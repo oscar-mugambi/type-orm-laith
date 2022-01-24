9f3371419a5d978b405d9ef563bab0bded524802
oscar-mugambi <oscarmugambi@gmail.com>
2022-01-24 20:52:08 +0300

    implemented query using query builder

--- dist/routes/fetch_clients.js
+++ dist/routes/fetch_clients.js
@@ -6,10 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.fetchClientRouter = void 0;
 const express_1 = __importDefault(require("express"));
 const Client_1 = require("../entities/Client");
+const typeorm_1 = require("typeorm");
 const router = express_1.default.Router();
 exports.fetchClientRouter = router;
 router.get('/api/clients', async (req, res) => {
-    const client = await Client_1.Client.find();
-    res.json(client);
+    const clients = await (0, typeorm_1.createQueryBuilder)('client')
+        .select('client')
+        .from(Client_1.Client, 'client')
+        .where('client.id = :clientId', { clientId: 2 })
+        .getOne();
+    res.json(clients);
 });
 //# sourceMappingURL=fetch_clients.js.map
--- src/routes/fetch_clients.ts
+++ src/routes/fetch_clients.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { Client } from '../entities/Client';
+import { createQueryBuilder } from 'typeorm';
 
 const router = express.Router();
 
 router.get('/api/clients', async (req, res) => {
-  const client = await Client.find();
-  res.json(client);
+  const clients = await createQueryBuilder('client')
+    .select('client')
+    .from(Client, 'client')
+    .where('client.id = :clientId', { clientId: 2 })
+    .getOne();
+  res.json(clients);
 });
 
 export { router as fetchClientRouter };
